dac44c91d07db1b3b82fcb6cbd97b07c54a5825d
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2026-06-29 23:43:51 +0300

    build: make macos architectures configurable
    Verification: B_BUILD_TYPE=Release B_MACOS_ARCHS="arm64;x86_64" ./clean_build.sh stopped before CMake because Homebrew Qt5 is not installed. Output ended with: Please install qt5.

--- clean_build.sh
+++ clean_build.sh
@@ -11,10 +11,10 @@ fi
 # default build configuration
 B_BUILD_TYPE=${B_BUILD_TYPE:-Debug}
 if [ "$(uname)" = "Darwin" ]; then
-    # OSX needs a lot of extra help, poor thing
-    # run the osx_environment.sh script to fix paths
     . ./osx_environment.sh
-    B_CMAKE_FLAGS="-DCMAKE_OSX_SYSROOT=$(xcode-select --print-path)/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 $B_CMAKE_FLAGS"
+    B_MACOS_DEPLOYMENT_TARGET=${B_MACOS_DEPLOYMENT_TARGET:-11.0}
+    B_MACOS_ARCHS=${B_MACOS_ARCHS:-arm64}
+    B_CMAKE_FLAGS="-DCMAKE_OSX_SYSROOT=$(xcode-select --print-path)/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -DCMAKE_OSX_DEPLOYMENT_TARGET=$B_MACOS_DEPLOYMENT_TARGET -DCMAKE_OSX_ARCHITECTURES=$B_MACOS_ARCHS $B_CMAKE_FLAGS"
 fi
 # allow local customizations to build environment
 [ -r ./build_env.sh ] && . ./build_env.sh
